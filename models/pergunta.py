d8556dae0830f693ab90c57377b114be87b3acd8
Gabriela Rocha Conrado <113737074+gabirc26@users.noreply.github.com>
2024-04-19 08:48:21 -0300

    adicionando perguntas

--- models/pergunta.py
+++ models/pergunta.py
@@ -19,3 +19,36 @@ class Pergunta:
                             print("Gênero deve ser 'M' para masculino ou 'F' para feminino. Por favor, tente novamente.")
                         else:
                             break
+                            
+                perguntas = [
+                    "\n1 - Você possui algum tipo de seguro atualmente?\n ",
+                    "\n2 - Você está ciente das diversas modalidades de seguros disponíveis no mercado?\n ",
+                    "\n3 - Você considera o seguro um investimento importante?\n ",
+                    "\n4 - Você acredita que um seguro oferece um bom custo-benefício?\n ",
+                    "\n5 - Você está familiarizado com o conceito de apólice de seguros?\n ",
+                    "\n6- Você gostaria de receber informações sobre seguros?\n "
+                ]
+                
+                respostas_perguntas = []
+                for pergunta in perguntas:
+                    while True:
+                        try:
+                            resposta = int(input(pergunta))
+                            if resposta in [1, 2, 3]:
+                                respostas_perguntas.append(resposta)
+                                break
+                            else:
+                                print("Resposta inválida. Por favor, insira 1 para Sim, 2 para Não ou 3 para Não sei.")
+                        except ValueError:
+                            print("Entrada inválida. Por favor, insira um número.")
+                
+                data_e_hora= datetime.now()
+                data = data_e_hora.strftime("%d/%m/%Y %H:%M")
+                resposta = Dados(idade, genero, respostas_perguntas, data)
+                respostas.append(resposta)
+                
+            except ValueError:
+                print("Entrada inválida. Por favor, insira um número para a idade.")
+                continue
+
+        return respostas
